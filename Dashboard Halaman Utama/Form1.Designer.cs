43e1d39a2b98006b4c1d91f5a12026305f94b9b4
Nizar Arifin <nizararifin07@gmail.com>
2024-06-08 21:29:22 +0700

    Halaman Dashboard

---
+++ Dashboard Halaman Utama/Form1.Designer.cs
@@ -0,0 +1,455 @@
+﻿namespace Dashboard_Halaman_Utama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel4 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel6 = new Panel();
+            panel5 = new Panel();
+            textBox1 = new TextBox();
+            panel7 = new Panel();
+            textBox2 = new TextBox();
+            panel8 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
+            panel11 = new Panel();
+            panel12 = new Panel();
+            panel13 = new Panel();
+            panel14 = new Panel();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            panel15 = new Panel();
+            panel16 = new Panel();
+            panel17 = new Panel();
+            panel18 = new Panel();
+            panel19 = new Panel();
+            panel20 = new Panel();
+            panel21 = new Panel();
+            panel22 = new Panel();
+            panel23 = new Panel();
+            panel24 = new Panel();
+            panel25 = new Panel();
+            panel26 = new Panel();
+            panel27 = new Panel();
+            panel28 = new Panel();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel6.SuspendLayout();
+            panel5.SuspendLayout();
+            panel7.SuspendLayout();
+            panel8.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Highlight;
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Location = new Point(27, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(27, 27);
+            panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.BackgroundImage = (Image)resources.GetObject("panel4.BackgroundImage");
+            panel4.Location = new Point(12, 130);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(318, 101);
+            panel4.TabIndex = 4;
+            panel4.Paint += panel4_Paint;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonHighlight;
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.Controls.Add(panel18);
+            panel2.Controls.Add(panel17);
+            panel2.Controls.Add(panel16);
+            panel2.Controls.Add(panel15);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(panel14);
+            panel2.Location = new Point(12, 273);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(318, 83);
+            panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
+            panel3.Controls.Add(panel24);
+            panel3.Controls.Add(panel23);
+            panel3.Controls.Add(panel22);
+            panel3.Controls.Add(panel21);
+            panel3.Controls.Add(textBox4);
+            panel3.Controls.Add(panel13);
+            panel3.Location = new Point(12, 374);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(318, 83);
+            panel3.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            panel6.BackgroundImage = (Image)resources.GetObject("panel6.BackgroundImage");
+            panel6.Controls.Add(panel28);
+            panel6.Controls.Add(panel27);
+            panel6.Controls.Add(panel26);
+            panel6.Controls.Add(panel25);
+            panel6.Controls.Add(textBox5);
+            panel6.Controls.Add(panel12);
+            panel6.Location = new Point(12, 476);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(318, 83);
+            panel6.TabIndex = 7;
+            // 
+            // panel5
+            // 
+            panel5.BackgroundImage = (Image)resources.GetObject("panel5.BackgroundImage");
+            panel5.Controls.Add(textBox1);
+            panel5.Location = new Point(7, 237);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(328, 332);
+            panel5.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(5, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(129, 27);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Yang Akan Datang";
+            // 
+            // panel7
+            // 
+            panel7.BackgroundImage = (Image)resources.GetObject("panel7.BackgroundImage");
+            panel7.Controls.Add(textBox2);
+            panel7.Location = new Point(7, 93);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(328, 141);
+            panel7.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(5, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(139, 27);
+            textBox2.TabIndex = 10;
+            textBox2.Text = "Pengingat Waktu Ini";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // panel8
+            // 
+            panel8.BackgroundImage = (Image)resources.GetObject("panel8.BackgroundImage");
+            panel8.Controls.Add(panel11);
+            panel8.Controls.Add(panel10);
+            panel8.Controls.Add(panel9);
+            panel8.Controls.Add(panel1);
+            panel8.Location = new Point(0, 703);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(344, 51);
+            panel8.TabIndex = 10;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = SystemColors.Highlight;
+            panel9.BackgroundImage = (Image)resources.GetObject("panel9.BackgroundImage");
+            panel9.Location = new Point(109, 12);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(32, 27);
+            panel9.TabIndex = 11;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = SystemColors.Highlight;
+            panel10.BackgroundImage = (Image)resources.GetObject("panel10.BackgroundImage");
+            panel10.Location = new Point(196, 12);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(26, 36);
+            panel10.TabIndex = 11;
+            // 
+            // panel11
+            // 
+            panel11.BackColor = SystemColors.Highlight;
+            panel11.BackgroundImage = (Image)resources.GetObject("panel11.BackgroundImage");
+            panel11.Location = new Point(274, 11);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(30, 28);
+            panel11.TabIndex = 11;
+            // 
+            // panel12
+            // 
+            panel12.BackColor = SystemColors.Highlight;
+            panel12.BackgroundImage = (Image)resources.GetObject("panel12.BackgroundImage");
+            panel12.Location = new Point(15, 3);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(73, 77);
+            panel12.TabIndex = 11;
+            // 
+            // panel13
+            // 
+            panel13.BackColor = SystemColors.Highlight;
+            panel13.BackgroundImage = (Image)resources.GetObject("panel13.BackgroundImage");
+            panel13.Location = new Point(28, 12);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(45, 59);
+            panel13.TabIndex = 11;
+            // 
+            // panel14
+            // 
+            panel14.BackColor = SystemColors.Highlight;
+            panel14.BackgroundImage = (Image)resources.GetObject("panel14.BackgroundImage");
+            panel14.Location = new Point(28, 17);
+            panel14.Name = "panel14";
+            panel14.Size = new Size(51, 49);
+            panel14.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.HotTrack;
+            textBox3.Location = new Point(211, 28);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(90, 27);
+            textBox3.TabIndex = 11;
+            textBox3.Text = "11.00-13.00";
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.HotTrack;
+            textBox4.Location = new Point(211, 28);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(90, 27);
+            textBox4.TabIndex = 12;
+            textBox4.Text = "20.00-22.00";
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.HotTrack;
+            textBox5.Location = new Point(211, 27);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(90, 27);
+            textBox5.TabIndex = 13;
+            textBox5.Text = "23.00-01.00";
+            // 
+            // panel15
+            // 
+            panel15.BackColor = SystemColors.Highlight;
+            panel15.BackgroundImage = (Image)resources.GetObject("panel15.BackgroundImage");
+            panel15.Location = new Point(97, 17);
+            panel15.Name = "panel15";
+            panel15.Size = new Size(66, 13);
+            panel15.TabIndex = 11;
+            // 
+            // panel16
+            // 
+            panel16.BackColor = SystemColors.Highlight;
+            panel16.BackgroundImage = (Image)resources.GetObject("panel16.BackgroundImage");
+            panel16.Location = new Point(97, 28);
+            panel16.Name = "panel16";
+            panel16.Size = new Size(32, 13);
+            panel16.TabIndex = 11;
+            // 
+            // panel17
+            // 
+            panel17.BackColor = SystemColors.Highlight;
+            panel17.BackgroundImage = (Image)resources.GetObject("panel17.BackgroundImage");
+            panel17.Location = new Point(97, 44);
+            panel17.Name = "panel17";
+            panel17.Size = new Size(86, 11);
+            panel17.TabIndex = 11;
+            // 
+            // panel18
+            // 
+            panel18.BackColor = SystemColors.Highlight;
+            panel18.BackgroundImage = (Image)resources.GetObject("panel18.BackgroundImage");
+            panel18.Location = new Point(97, 55);
+            panel18.Name = "panel18";
+            panel18.Size = new Size(83, 11);
+            panel18.TabIndex = 11;
+            // 
+            // panel19
+            // 
+            panel19.BackgroundImage = (Image)resources.GetObject("panel19.BackgroundImage");
+            panel19.Location = new Point(306, 12);
+            panel19.Name = "panel19";
+            panel19.Size = new Size(24, 21);
+            panel19.TabIndex = 11;
+            // 
+            // panel20
+            // 
+            panel20.BackgroundImage = (Image)resources.GetObject("panel20.BackgroundImage");
+            panel20.Location = new Point(315, 34);
+            panel20.Name = "panel20";
+            panel20.Size = new Size(5, 5);
+            panel20.TabIndex = 12;
+            // 
+            // panel21
+            // 
+            panel21.BackColor = SystemColors.Highlight;
+            panel21.BackgroundImage = (Image)resources.GetObject("panel21.BackgroundImage");
+            panel21.Location = new Point(97, 12);
+            panel21.Name = "panel21";
+            panel21.Size = new Size(75, 13);
+            panel21.TabIndex = 13;
+            // 
+            // panel22
+            // 
+            panel22.BackColor = SystemColors.Highlight;
+            panel22.BackgroundImage = (Image)resources.GetObject("panel22.BackgroundImage");
+            panel22.Location = new Point(97, 28);
+            panel22.Name = "panel22";
+            panel22.Size = new Size(32, 10);
+            panel22.TabIndex = 13;
+            // 
+            // panel23
+            // 
+            panel23.BackColor = SystemColors.Highlight;
+            panel23.BackgroundImage = (Image)resources.GetObject("panel23.BackgroundImage");
+            panel23.Location = new Point(97, 43);
+            panel23.Name = "panel23";
+            panel23.Size = new Size(50, 12);
+            panel23.TabIndex = 13;
+            // 
+            // panel24
+            // 
+            panel24.BackColor = SystemColors.Highlight;
+            panel24.BackgroundImage = (Image)resources.GetObject("panel24.BackgroundImage");
+            panel24.Location = new Point(97, 61);
+            panel24.Name = "panel24";
+            panel24.Size = new Size(78, 12);
+            panel24.TabIndex = 13;
+            // 
+            // panel25
+            // 
+            panel25.BackColor = SystemColors.Highlight;
+            panel25.BackgroundImage = (Image)resources.GetObject("panel25.BackgroundImage");
+            panel25.Location = new Point(97, 14);
+            panel25.Name = "panel25";
+            panel25.Size = new Size(73, 11);
+            panel25.TabIndex = 13;
+            // 
+            // panel26
+            // 
+            panel26.BackColor = SystemColors.Highlight;
+            panel26.BackgroundImage = (Image)resources.GetObject("panel26.BackgroundImage");
+            panel26.Location = new Point(97, 31);
+            panel26.Name = "panel26";
+            panel26.Size = new Size(16, 10);
+            panel26.TabIndex = 13;
+            // 
+            // panel27
+            // 
+            panel27.BackColor = SystemColors.Highlight;
+            panel27.BackgroundImage = (Image)resources.GetObject("panel27.BackgroundImage");
+            panel27.Location = new Point(97, 47);
+            panel27.Name = "panel27";
+            panel27.Size = new Size(36, 12);
+            panel27.TabIndex = 13;
+            // 
+            // panel28
+            // 
+            panel28.BackColor = SystemColors.Highlight;
+            panel28.BackgroundImage = (Image)resources.GetObject("panel28.BackgroundImage");
+            panel28.Location = new Point(97, 65);
+            panel28.Name = "panel28";
+            panel28.Size = new Size(78, 10);
+            panel28.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(342, 754);
+            Controls.Add(panel20);
+            Controls.Add(panel19);
+            Controls.Add(panel8);
+            Controls.Add(panel6);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel4);
+            Controls.Add(panel5);
+            Controls.Add(panel7);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel8.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel4;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel6;
+        private Panel panel5;
+        private TextBox textBox1;
+        private Panel panel7;
+        private TextBox textBox2;
+        private Panel panel8;
+        private Panel panel9;
+        private Panel panel11;
+        private Panel panel10;
+        private Panel panel12;
+        private Panel panel14;
+        private Panel panel13;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Panel panel18;
+        private Panel panel17;
+        private Panel panel16;
+        private Panel panel15;
+        private Panel panel21;
+        private Panel panel19;
+        private Panel panel20;
+        private Panel panel24;
+        private Panel panel23;
+        private Panel panel22;
+        private Panel panel25;
+        private Panel panel26;
+        private Panel panel28;
+        private Panel panel27;
+    }
+}
---
+++ Dashboard Halaman Utama/Form1.cs
@@ -0,0 +1,35 @@
+namespace Dashboard_Halaman_Utama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
